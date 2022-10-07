f74a3df892e12654d691844a55b1eebda1eda05c
Chirag Mangla <chirag.mangla@chanel.com>
2022-10-07 21:48:44 +0530

    Data Wrangling

---
+++ Case Study #2 - Pizza Runner/Data Wrangling.sql
@@ -0,0 +1,64 @@
+-- Data Wrangling --
+
+-- custmer_orders --
+-- Remove null values from columns : exclusions, extras
+
+update customer_orders
+set exclusions='' where exclusions= 'null' or exclusions is null
+
+update customer_orders
+set extras='' where extras= 'null' or extras is null
+
+-- Verify the clean table
+select *
+from customer_orders
+
+-- runner_orders --
+-- Remove null values from:
+	-- pickup_time
+	--distance
+	-- duration
+	--cancellation
+
+update runner_orders
+set cancellation='' where cancellation= 'null' or cancellation is null
+
+update runner_orders
+set distance=null where distance='null'
+
+update runner_orders
+set duration=null where duration='null'
+
+update runner_orders
+set pickup_time= null where pickup_time='null'
+
+-- Remove extra text from colums:
+	-- distance
+	-- duration
+
+update runner_orders
+set distance=trim('km' from distance)
+
+update runner_orders
+set duration=trim('minutes' from duration)
+
+--Alter column data types for:
+	--pickup_time -> timestamp without time zone (DATETIME)
+	-- distance -> float
+	-- duration -> int
+
+ALTER TABLE RUNNER_ORDERS
+ALTER COLUMN PICKUP_TIME type timestamp WITHOUT TIME ZONE
+using PICKUP_TIME::timestamp without time zone
+
+ALTER TABLE RUNNER_ORDERS
+ALTER COLUMN distance type double precision
+using distance::double precision
+
+ALTER TABLE RUNNER_ORDERS
+ALTER COLUMN duration type int
+using duration::integer
+
+-- Verify the clean table
+select *
+from runner_orders
